7745ef2e5c0e1465c18c2e03db790017a22143b6
hisashi0315 <g17h.tagai@ikubun.com>
2019-05-03 09:11:02 +0900

    editing  function had added!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,9 +17,14 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post .update(post_params)
+    redirect_to @post
   end
 
   def destroy
